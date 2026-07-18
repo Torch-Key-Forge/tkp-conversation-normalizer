3cc362da90d420bb05980914a55cadcb9fb3db7f
Carl Burke <smallplotnursery@gmail.com>
2026-07-18 18:21:01 -0400

    Add package initializer

---
+++ src/tkp_conversation_normalizer/__init__.py
@@ -0,0 +1,6 @@
+"""TKP Conversation Normalizer public package."""
+
+from .normalizer import normalize_conversation, normalize_export
+
+__all__ = ["normalize_conversation", "normalize_export"]
+__version__ = "0.1.0"
